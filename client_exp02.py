9e684f339a55c7dbd358f5337d5b0355d02bd4d2
raycho <raycho521@yonsei.ac.kr>
2022-11-03 20:17:22 +0900

    feat : edit exp 2

--- client_exp02.py
+++ client_exp02.py
@@ -3,19 +3,23 @@ import os
 import sys
 import time
 
-Host = '165.132.5.137'
+Host = '192.168.0.7'
 Port = 6789
 
+# AF_INET : ipv4 주소 사용
+# SOCK_STREAM : TCP/IP 프로토콜 사용
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(("", Port))
+clientSocket.connect((Host, Port))
 
 message = input("요청할 파일 이름 : ")
 clientSocket.sendall(message.encode())
 
 data = clientSocket.recv(4096)
 data_iter = 0
+
+# 불러온 데이터를 저장할 경로 지정
 os.chdir("/Users/raycho/Documents/yoonhocho/2022/coding_edu/socket_python/client")
-nowdir = os.getcwd()
+nowdir = os.getcwd() # 현재의 경로 저장
 
 with open(nowdir+"/"+message, "wb") as f:
     try:
--- webserver_exp02.py
+++ webserver_exp02.py
@@ -2,28 +2,35 @@ from socket import *
 import sys
 import time
 
+# AF_INET : ipv4 주소 사용
+# SOCK_STREAM : TCP/IP 프로토콜 사용
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
 serverPort = 6789
+
+# port number를 이용하여 소켓과 서버 포트를 연결한 뒤, client의 연결을 기다린다.
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 while True:
     print('The server is ready to receive')
+
     clientSocket, addr = serverSocket.accept()
+    print("Connected by ", addr)
 
     try:
-        message = clientSocket.recv(4096)
+        message = clientSocket.recv(4096) # 한번에 최대 4096바이트만 읽어오도록 한다.
         print('Received from', addr, message)
         filename = message
-        f = open(filename)
+        f = open(filename) # 요청한 파일 경로를 통해 파일을 읽는다.
         data_iter = 0
         with open(filename, 'rb') as f:
             try:
-                outputdata = f.read(1024)
+                outputdata = f.read(4096)  # 한번에 4096 바이트씩 읽어온다.
                 while outputdata:
                     clientSocket.send(outputdata) # 데이터를 보낸다.
                     data_iter = data_iter + 1
-                    outputdata = f.read(1024) # 4096 바이트를 읽는다.
+                    outputdata = f.read(4096) # 4096 바이트를 읽는다.
                     print("파일 %s (seq: %d) is sent" % (filename, data_iter))
                     time.sleep(5)
             except Exception as ex:
