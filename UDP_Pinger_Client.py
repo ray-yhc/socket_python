d7067d9e4b5a7585b6ebea6dfac62b03be2c005b
raycho <raycho521@yonsei.ac.kr>
2022-11-03 20:55:03 +0900

    feat : edit exp 3

--- UDP_Pinger_Client.py
+++ UDP_Pinger_Client.py
@@ -6,7 +6,10 @@ host = argv[1]
 port = argv[2]
 timeout = 3.5
 
+# AF_INET : ipv4 주소 사용
+# SOCK_DGRAM : UDP/IP 프로토콜 사용
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+# timeout 시간 설정
 clientSocket.settimeout(timeout)
 
 port = int(port)
@@ -17,16 +20,22 @@ while ptime<10:
     data = "Ping " + str(ptime) + " " + time.asctime()
 
     try :
-        RTTb = time.time()
-        clientSocket.sendto(data.encode(), (host,port))
-        message, address = clientSocket.recvfrom(1024)
-        RTTa = time.time()
+        RTTb = time.time() # 전송 시작 시각 측정
+
+        clientSocket.sendto(data.encode(), (host,port)) # 송신
+        message, address = clientSocket.recvfrom(1024) # 수신
+
+        RTTa = time.time() # 수신 도착 시각 측정
         print("Reply from" + address[0] + ": " + message.decode())
+
+        # 총 소요된 RTT 측정
         RTT = RTTa-RTTb
-        print("RTT : %.4f msec" %(RTT))
+        print("RTT : %.4f msec\n" %(RTT))
+
+
     except Exception as ex :
         # print(ex)
-        print("Request timed out.")
+        print("Request timed out.\n")
         continue
 clientSocket.close()
 
--- UDP_Pinger_Server.py
+++ UDP_Pinger_Server.py
@@ -1,16 +1,22 @@
 from socket import *
 import random
 
+# AF_INET : ipv4 주소 사용
+# SOCK_DGRAM : UDP/IP 프로토콜 사용
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverPort = 6785
 serverSocket.bind(('', serverPort))
 
 while True:
     rand = random.randint(0,10)
+
     message, address = serverSocket.recvfrom(1024)
     message = message.upper()
 
+    # rand가  4 미만일 경우 packet loss 가 발생했다고 간주하고 넘어간다.(40% 확률)
     if rand < 4:
         continue
 
+    # 받은 메시지를 다시 재전송
     serverSocket.sendto(message, address)
+
